56236b05ea3ee6b71f9d08cc2eba4c0312a207ea
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2021-11-03 09:04:36 +0700

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 class Game {
-  contructor
+  contructor(w,h){
+    this.w = w
+    this.h = h
+    
+    this.canvas = document.createElement('canvas')
+    this.canvas.width = this.w
+    this.canvas.height = this.h
+    
+    document.body.appendChild(this.canvas)
+  }
 }
